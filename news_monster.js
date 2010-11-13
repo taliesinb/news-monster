31b377e8224e68a3d5ebc52e620923590ce41be4
bobbylox <bobbylox>
2010-11-13 00:55:10 -0600

    mistakes abound

--- news_monster.js
+++ news_monster.js
@@ -17,39 +17,36 @@ if(typeof Object.create !== "function"){
 
 /*function processKeyDown(e){
    if(e.keyCode === 37){ //left
-      GAME.circle.dx = -55;
+      GAME.monster.dx += GAME.elapsed*dr;
    }
-   if(e.keyCode === 38){ //up
-      GAME.circle.dy = -55;
-   }
-   if(e.keyCode === 39){ //right
-      GAME.circle.dx = 55;
-   }
-   if(e.keyCode === 40){ //down
-      GAME.circle.dy = 55;
-   }
-}*/
 
-/*function processKeyUp(e){
-   GAME.circle.dx = 0;
-   GAME.circle.dy = 0;
-}*/
+   if(e.keyCode === 39){ //right
+      GAME.monster.arc += -GAME.elapsed*dr;
+   }
+}
 
 GAME.draw_bg = function (){
+	
    ctxt.strokeStyle='#000000';
    ctxt.fillStyle='#000000';
    ctxt.fillRect(0,0,stage_width,stage_height);
 }
 
-GAME.circle = {
-	x:10, y:10, 
-	dx:50, dy:50, 
-	ddx:0, ddy:0,//500,
-	radius:10
-};
-
-GAME.circle.draw = function(){
-   ctxt.shadowColor = '#FFFFFF'
+/*GAME.monster = {
+    arcRadius:100.0,
+    x:(stage_width/2-arcRadius), 
+    y:stage_width/2, 
+    dx:0, 
+    dy:0, 
+    ddx:0, 
+    ddy:0,//500,
+    radius:10,
+    arc: Math.PI,
+    dr:1
+}
+/*
+GAME.monster.draw = function(){
+   ctxt.shadowColor = '#FFFFFF';
    ctxt.strokeStyle = '#FFFFFF';
    ctxt.fillStyle = '#FFFF00';
    ctxt.shadowBlur = 40;
@@ -59,28 +56,12 @@ GAME.circle.draw = function(){
    ctxt.fill();
 }
 
-GAME.circle.update = function(){
+/*GAME.monster.update = function(){
 
-   this.dx += this.ddx*GAME.elapsed;
-   this.dy += this.ddy*GAME.elapsed;
-
-   
-
-   this.x += this.dx*GAME.elapsed;
-   this.y += this.dy*GAME.elapsed;
-
-   if((this.x + this.radius > stage_width && this.dx > 0)||
-      (this.x - this.radius < 0 && this.dx < 0)){
-         this.dx = -this.dx;
-   }
-
-   if((this.y + this.radius > stage_height && this.dy > 0)|| 
-       (this.y - this.radius < 0 && this.dy < 0)){
-      this.dy = -this.dy;
-   }
-
+    this.x = Math.cos(this.arcRadius);
+    this.y = Math.sin(this.arcRadius);
 }
-
+*/
 
 GAME.play = function(){
    var d = new Date();
@@ -89,8 +70,8 @@ GAME.play = function(){
    GAME.elapsed = (timeNow-GAME.previousTime)/1000;
    
    GAME.draw_bg();
-   GAME.circle.update();
-   GAME.circle.draw();
+   //GAME.monster.update();
+   //GAME.monster.draw();
 
    GAME.previousTime = timeNow;
 }
